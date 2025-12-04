55811137482b6acda8ddf170347c58e1027c9cb9
Noshin <bsse1503@iit.du.ac.bd>
2025-12-04 12:50:18 +0600

    added welcome_pages folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 
+import WelcomeScreen from './pages/welcome_pages/WelcomeScreen';
 import HomePage from './HomePage';
 import ResultPage from './ResultPage';
 
+// Welcome Page Wrapper
+const WelcomePage = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Navigate to home page after login (you can add LoginScreen route later)
+    navigate('/home');
+  };
+
+  const handleContinueWithoutLogin = () => {
+    // Navigate to home page without login
+    navigate('/home');
+  };
+
+  return (
+    <WelcomeScreen 
+      onLogin={handleLogin}
+      onContinueWithoutLogin={handleContinueWithoutLogin}
+    />
+  );
+};
+
 function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element = {<HomePage/>}></Route>
-                <Route path="/result-page" element = {<ResultPage/>}></Route>
-            </Routes>
-        </Router>
-    )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        {/* <Route path="/home" element={<HomePage />} />
+        <Route path="/result-page" element={<ResultPage />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
